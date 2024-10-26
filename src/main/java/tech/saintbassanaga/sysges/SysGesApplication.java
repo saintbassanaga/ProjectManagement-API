40054a0c391428d586834a3a6fcfbe179da4ba9e
saintbassanaga <saintbassanaga01@gmail.com>
2024-10-26 05:09:41 +0100

    init: Initial commit
    this commit is for project repository initialisation

---
+++ src/main/java/tech/saintbassanaga/sysges/SysGesApplication.java
@@ -0,0 +1,13 @@
+package tech.saintbassanaga.sysges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysGesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysGesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/saintbassanaga/sysges/SysGesApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.saintbassanaga.sysges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysGesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
